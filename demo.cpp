5ac28b3a2994db7621d6bdfda5f0c7e16c2ddb2f
Mingjie Li <limingjie@outlook.com>
2015-05-28 23:41:53 +0800

    window w/o border and has background color

--- demo.cpp
+++ demo.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char **argv)
 {
     MainWindow *window = new MainWindow(100, 100, 600, 300, "Foo bar");
+    window->color(fl_rgb_color(80, 140, 220));
+    window->clear_border();
     window->show(argc, argv);
     return Fl::run();
 }
